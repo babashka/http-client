6cf4daca7e03309a776f7b37f9897e878563ee5f
Michiel Borkent <michielborkent@gmail.com>
2023-07-05 20:42:11 +0200

    Fix babashka/babashka#1587: make :insecure more insecure

--- src/babashka/http_client/internal.clj
+++ src/babashka/http_client/internal.clj
@@ -22,7 +22,7 @@
    [java.time Duration]
    [java.util.concurrent CompletableFuture]
    [java.util.function Function Supplier]
-   [javax.net.ssl KeyManagerFactory TrustManagerFactory SSLContext TrustManager X509TrustManager]))
+   [javax.net.ssl KeyManagerFactory TrustManagerFactory SSLContext TrustManager X509ExtendedTrustManager #_X509TrustManager]))
 
 (set! *warn-on-reflection* true)
 
@@ -49,10 +49,15 @@
       (doto (KeyStore/getInstance store-type)
         (.load kss (char-array store-pass))))))
 
-(def insecure-tm (reify X509TrustManager
-                   (checkClientTrusted [_ _ _])
-                   (checkServerTrusted [_ _ _])
-                   (getAcceptedIssuers [_] (into-array X509Certificate []))))
+(def insecure-tm (delay
+                   (proxy [X509ExtendedTrustManager] []
+                     (checkClientTrusted
+                       ([_ _])
+                       ([_ _ _]))
+                     (checkServerTrusted
+                       ([_ _])
+                       ([_ _ _]))
+                     (getAcceptedIssuers [] (into-array X509Certificate [])))))
 
 (defn ->SSLContext
   [v]
@@ -67,7 +72,7 @@
                                             (.init ks (char-array key-store-pass)))))
 
           trust-managers (if insecure
-                           (into-array TrustManager [insecure-tm])
+                           (into-array TrustManager [@insecure-tm])
                            (when-let [ts (load-keystore trust-store trust-store-type trust-store-pass)]
                              (.getTrustManagers (doto (TrustManagerFactory/getInstance (TrustManagerFactory/getDefaultAlgorithm))
                                                   (.init ts)))))]
