ae498b5c41812294760aceaaf93f6b784ef5e1bf
Michiel Borkent <michielborkent@gmail.com>
2023-09-04 15:02:28 +0200

    Fix #43: when using a string key for `Accept` header, the value is overridden by the default (#44)

--- src/babashka/http_client/internal/helpers.clj
+++ src/babashka/http_client/internal/helpers.clj
@@ -1,5 +1,6 @@
 (ns babashka.http-client.internal.helpers
-  {:no-doc true})
+  {:no-doc true}
+  (:require [clojure.string :as str]))
 
 (defn ->uri [uri]
   (cond (string? uri) (java.net.URI/create uri)
@@ -20,6 +21,23 @@
     (-> k str (subs 1))
     (str k)))
 
+(defn capitalize-header [hdr]
+  (str/join "-" (map str/capitalize (str/split hdr #"-"))))
+
+(defn prefer-string-keys
+  "Dissoc-es keyword header if equivalent string header is available already."
+  [header-map]
+  (reduce (fn [m k]
+            (if (keyword? k)
+              (let [s (coerce-key k)]
+                (if (or (clojure.core/get header-map (capitalize-header s))
+                        (clojure.core/get header-map s))
+                  (dissoc m k)
+                  m))
+              m))
+          header-map
+          (keys header-map)))
+
 (defn coerce-headers
   [headers]
   (mapcat
@@ -28,3 +46,9 @@
        (interleave (repeat (coerce-key k)) v)
        [(coerce-key k) v]))
    headers))
+
+;;;;
+
+(comment
+  (prefer-string-keys {:accept 1 "Accept" 2})
+  )
